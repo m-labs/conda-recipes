26fa61b54eb3bc4494f01853b3d78dd6170df8fb
whitequark <whitequark@whitequark.org>
2015-10-15 19:01:17 +0300

    llvm-or1k: force -m32 on i686.

--- conda/llvm-or1k/build.sh
+++ conda/llvm-or1k/build.sh
@@ -5,6 +5,9 @@ git clone https://github.com/openrisc/clang-or1k clang --depth 1
 cd ..
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DLLVM_TARGETS_TO_BUILD="OR1K;X86" -DCMAKE_BUILD_TYPE=Rel -DLLVM_ENABLE_ASSERTIONS=ON
+if [ $(uname -m) = "i686" ]; then
+  COMPILER32="-DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32"
+fi
+cmake .. $COMPILER32 -DCMAKE_INSTALL_PREFIX=$PREFIX -DLLVM_TARGETS_TO_BUILD="OR1K;X86" -DCMAKE_BUILD_TYPE=Rel -DLLVM_ENABLE_ASSERTIONS=ON
 make -j2
 make install
