e4a514c8786922e0fc53f77e1377b5eeb820a373
whitequark <whitequark@whitequark.org>
2015-10-15 08:29:54 +0300

    libgit2: force -m32 on i686.

--- conda/libgit2/build.sh
+++ conda/libgit2/build.sh
@@ -2,6 +2,9 @@
 
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX
+if [ $(uname -m) = "i686" ]; then
+  COMPILER32="-DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32"
+fi
+cmake .. $COMPILER32 -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX
 make -j2
 make install
