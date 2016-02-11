d7189df6ad5eae09493e15ec87e4dde47584bef2
whitequark <whitequark@whitequark.org>
2016-02-11 10:29:32 +0000

    binutils-or1k-linux: take Windows PATH convention into account.

--- conda/binutils-or1k-linux/build.sh
+++ conda/binutils-or1k-linux/build.sh
@@ -1,6 +1,6 @@
 patch -p1 < $RECIPE_DIR/../../misc/binutils-2.25.1-or1k-R_PCREL-pcrel_offset.patch
 mkdir build
 cd build
-../configure --target=or1k-linux --prefix=$PREFIX
+../configure --target=or1k-linux --prefix=$PREFIX/Library
 make -j2
 make install
