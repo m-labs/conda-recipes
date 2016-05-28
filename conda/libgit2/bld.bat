f671496438e84c8c00bce59ca90bdfe4f596521d
whitequark <whitequark@whitequark.org>
2016-05-28 18:57:36 +0000

    libgit2: install DLL in the proper place (Library/bin).

--- conda/libgit2/bld.bat
+++ conda/libgit2/bld.bat
@@ -6,7 +6,7 @@ set CMAKE_GENERATOR=Visual Studio 14 2015
 ) else (
 set CMAKE_GENERATOR=Visual Studio 14 2015 Win64
 )
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%" -DSTDCALL=OFF -DSTATIC_CRT=OFF -DCMAKE_PREFIX_PATH="%PREFIX%" "%SRC_DIR%"
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%" -DSTDCALL=OFF -DSTATIC_CRT=OFF -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
 if errorlevel 1 exit 1
 REM Build step
 cmake --build .
@@ -14,6 +14,3 @@ if errorlevel 1 exit 1
 REM Install step
 cmake --build . --target install
 if errorlevel 1 exit 1
-REM Hack to help pygit2 to find libgit2
-mkdir "%PREFIX%\Scripts"
-copy "%PREFIX%\bin\git2.dll" "%PREFIX%\Scripts\"
