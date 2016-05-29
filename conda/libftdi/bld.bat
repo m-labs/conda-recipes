4a73a79a88963834da2c02c0f7c6dbacb2e38ec2
whitequark <whitequark@whitequark.org>
2016-05-29 18:11:43 +0000

    libftdi: build with MinGW.

--- conda/libftdi/bld.bat
+++ conda/libftdi/bld.bat
@@ -2,12 +2,12 @@ mkdir build
 cd build
 REM Configure step
 if "%ARCH%"=="32" (
-set CMAKE_GENERATOR=Visual Studio 14 2015
+set MINGW=%MINGW32%
 ) else (
-set CMAKE_GENERATOR=Visual Studio 14 2015 Win64
+set MINGW=%MINGW64%
 )
-set CMAKE_GENERATOR_TOOLSET=v140
-cmake -G "%CMAKE_GENERATOR%" -T %CMAKE_GENERATOR_TOOLSET% -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
+set PATH=%MINGW%;%PATH%
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
 if errorlevel 1 exit 1
 REM Build step
 cmake --build .
