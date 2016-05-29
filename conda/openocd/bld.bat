bb90b35158a665a396b8537aace06e86d801b743
whitequark <whitequark@whitequark.org>
2016-05-29 18:48:03 +0000

    openocd: add Windows support.

---
+++ conda/openocd/bld.bat
@@ -0,0 +1,30 @@
+REM Configure step
+if "%ARCH%"=="32" (
+set TOOLPREF=mingw-w64-i686-
+set TRIPLE=x86_64-pc-msys
+) else (
+set TOOLPREF=mingw-w64-x86_64-
+set TRIPLE=i686-pc-msys
+)
+set PATH=%MSYS%\usr\bin;%MSYS%\mingw%ARCH%\bin;%PATH%
+
+pacman -S --noconfirm %TOOLPREF%gcc %TOOLPREF%binutils make autoconf automake libtool pkg-config
+
+sh ./bootstrap
+if errorlevel 1 exit 1
+
+mkdir build
+cd build
+set LIBUSB1_CFLAGS=-I%PREFIX:\=/%/Library/include/libusb-1.0 ^
+  -DHAVE_LIBUSB_ERROR_NAME=1 -DHAVE_LIBUSB_GET_PORT_NUMBERS=1
+set LIBUSB1_LIBS=-L%PREFIX:\=/%/Library/lib -lusb-1.0
+sh ../configure --build=%TRIPLE% ^
+  --prefix="%PREFIX:\=/%/Library" --bindir="%PREFIX:\=/%/Scripts" ^
+  --enable-ftdi --disable-internal-libjaylink --disable-jlink
+if errorlevel 1 exit 1
+
+make
+if errorlevel 1 exit 1
+
+make install
+if errorlevel 1 exit 1
