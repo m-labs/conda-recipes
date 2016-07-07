1b5c7674fc3e1b054fa9784f79dd153b2b847cfc
whitequark <whitequark@whitequark.org>
2016-07-07 15:38:34 +0000

    libgit2, libssh2: actually build as Release on MSVC.

--- conda/libgit2/bld.bat
+++ conda/libgit2/bld.bat
@@ -6,11 +6,9 @@ set CMAKE_GENERATOR=Visual Studio 14 2015
 ) else (
 set CMAKE_GENERATOR=Visual Studio 14 2015 Win64
 )
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DSTDCALL=OFF -DSTATIC_CRT=OFF -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DSTDCALL=OFF -DSTATIC_CRT=OFF -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
 if errorlevel 1 exit 1
-REM Build step
-cmake --build .
+cmake --build . --config Release
 if errorlevel 1 exit 1
-REM Install step
-cmake --build . --target install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
--- conda/libssh2/build.sh
+++ conda/libssh2/build.sh
@@ -5,6 +5,9 @@ cd build
 if [ $(uname -m) = "i686" ]; then
   COMPILER32="-DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32"
 fi
-cmake .. $COMPILER32 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DOPENSSL_ROOT_DIR=$PREFIX -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_PREFIX_PATH=$PREFIX
-make -j2
-make install
+cmake .. $COMPILER32 -DCMAKE_INSTALL_PREFIX=$PREFIX -DOPENSSL_ROOT_DIR=$PREFIX -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_PREFIX_PATH=$PREFIX
+if errorlevel 1 exit 1
+cmake --build . --config Release
+if errorlevel 1 exit 1
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
