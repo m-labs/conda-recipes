33ec0705fd9ae07c1b98da437e3f80916bd61b60
whitequark <whitequark@whitequark.org>
2016-07-15 11:57:37 +0000

    rustc: fix --llvm-root (again).

--- conda/rustc/build.sh
+++ conda/rustc/build.sh
@@ -4,6 +4,6 @@ mkdir build
 cd build
 ../configure \
   --prefix=$PREFIX \
-  --llvm-root=$(which llvm-config)/../.. \
+  --llvm-root=$(dirname $(dirname $(which llvm-config))) \
   --disable-manage-submodules
 make install
