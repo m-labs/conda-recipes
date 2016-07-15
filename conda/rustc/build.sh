7b79e6149ead5e5bdde7deb9572ecf90c0ad7169
whitequark <whitequark@whitequark.org>
2016-07-15 11:31:49 +0000

    rustc: don't let build system manage submodules.

--- conda/rustc/build.sh
+++ conda/rustc/build.sh
@@ -4,5 +4,6 @@ mkdir build
 cd build
 ../configure \
   --prefix=$PREFIX \
-  --llvm-root=$(which llvm-config)/../..
+  --llvm-root=$(which llvm-config)/../.. \
+  --disable-manage-submodules
 make install
