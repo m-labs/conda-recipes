0f43c54fb820997cf37d5379cc66af37d5cadd36
whitequark <whitequark@whitequark.org>
2016-10-16 16:12:04 +0000

    rust-core-or1k: bump.

--- conda/rust-core-or1k/build.sh
+++ conda/rust-core-or1k/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 libs="libcore liballoc librustc_unicode libcollections liblibc_mini \
-  libunwind libpanic_unwind"
+  libunwind"
 destdir="${PREFIX}/lib/rustlib/or1k-unknown-none/lib/"
 rustc="rustc --out-dir ${destdir} --target or1k-unknown-none -g -O"
 
@@ -9,4 +9,5 @@ mkdir -p ${destdir}
 for lib in ${libs}; do
   ${rustc} src/${lib}/lib.rs
 done
-${rustc} -Cpanic=abort src/libpanic_abort/lib.rs
+${rustc} -Cpanic=abort  src/libpanic_abort/lib.rs
+${rustc} -Cpanic=unwind src/libpanic_unwind/lib.rs --cfg llvm_libunwind
