4a0d67e76d9fd4163b20068baa8db753e4c06615
whitequark <whitequark@whitequark.org>
2016-12-23 19:28:03 +0000

    rustc: update to 1.14.0.

--- conda/rustc/build.sh
+++ conda/rustc/build.sh
@@ -5,5 +5,7 @@ cd build
 ../configure \
   --prefix=$PREFIX \
   --llvm-root=$(dirname $(dirname $(which llvm-config))) \
-  --disable-manage-submodules
+  --disable-manage-submodules \
+  --enable-rustbuild \
+  --disable-llvm-static
 make install
