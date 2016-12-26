1891fe0a0cce16467023b57751a1df4765fabaf7
whitequark <whitequark@whitequark.org>
2016-12-26 05:25:45 +0000

    Revert "rustc: disable rustbuild, maybe."
    This reverts commit b7a089ad510e2c7a3c7ddd875bfc39cf7459b06c.

--- conda/rustc/build.sh
+++ conda/rustc/build.sh
@@ -5,5 +5,6 @@ cd build
 ../configure \
   --prefix=$PREFIX \
   --llvm-root=$(dirname $(dirname $(which llvm-config))) \
-  --disable-manage-submodules
+  --disable-manage-submodules \
+  --enable-rustbuild
 make install VERBOSE=1
