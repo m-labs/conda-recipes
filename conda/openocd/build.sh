95332e5ac1c93e459bb412bc6353719b9e0f9dd0
Robert Jordens <jordens@gmail.com>
2017-02-02 22:20:53 +0100

    openocd: rm more non-ascii files

--- conda/openocd/build.sh
+++ conda/openocd/build.sh
@@ -6,5 +6,6 @@
 make
 make install
 
-# conda chokes on non-ascii: 1986ве1т.cfg
+# conda chokes on non-ascii paths
+rm $PREFIX/share/openocd/scripts/target/?1879x?1?.cfg
 rm $PREFIX/share/openocd/scripts/target/1986??1?.cfg
