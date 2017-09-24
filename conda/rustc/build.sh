a219226b51fcd47bdb30eefd72ce8c53f7599bea
whitequark <whitequark@whitequark.org>
2017-09-24 17:33:04 +0000

    rustc: don't build docs.

--- conda/rustc/build.sh
+++ conda/rustc/build.sh
@@ -4,6 +4,7 @@ mkdir build
 cd build
 ../configure \
   --prefix=$PREFIX \
-  --llvm-root=$(dirname $(dirname $(which llvm-config)))
+  --llvm-root=$(dirname $(dirname $(which llvm-config))) \
+  --disable-docs
 ../x.py build
 ../x.py dist --install
