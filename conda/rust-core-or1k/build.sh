6ebdb328ff35a8e03e3136b1ceedd937bc867bc9
whitequark <whitequark@whitequark.org>
2017-12-26 14:41:22 +0000

    rust-core-or1k: update build script for rustc 1.22.1.

--- conda/rust-core-or1k/build.sh
+++ conda/rust-core-or1k/build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh -ex
 
-libs="libcore libstd_unicode liballoc liblibc_mini libunwind"
 destdir="${PREFIX}/lib/rustlib/or1k-unknown-none/lib/"
 rustc="rustc --out-dir ${destdir} -L ${destdir} --target or1k-unknown-none -g"
 rustc="${rustc} -C target-feature=+mul,+div,+ffl1,+cmov,+addc -C opt-level=s"
+rustc="${rustc} --crate-type rlib"
 
-mkdir -p ${destdir}
-for lib in ${libs}; do
-  ${rustc} src/${lib}/lib.rs
-done
-${rustc} -Cpanic=abort  src/libpanic_abort/lib.rs
-${rustc} -Cpanic=unwind src/libpanic_unwind/lib.rs --cfg llvm_libunwind
+${rustc} --crate-name core ../src/libcore/lib.rs
+${rustc} --crate-name std_unicode ../src/libstd_unicode/lib.rs
+${rustc} --crate-name alloc ../src/liballoc/lib.rs
+${rustc} --crate-name libc ../src/liblibc_mini/lib.rs
+${rustc} --crate-name unwind ../src/libunwind/lib.rs
+${rustc} -Cpanic=abort --crate-name panic_abort ../src/libpanic_abort/lib.rs
+${rustc} -Cpanic=unwind --crate-name panic_unwind ../src/libpanic_unwind/lib.rs \
+  --cfg llvm_libunwind
