35536bd9465cfc441336d1d3af4ba49f13c865fc
whitequark <whitequark@whitequark.org>
2017-12-26 15:15:19 +0000

    rust-core-or1k: fix paths.

--- conda/rust-core-or1k/build.sh
+++ conda/rust-core-or1k/build.sh
@@ -5,11 +5,11 @@ rustc="rustc --out-dir ${destdir} -L ${destdir} --target or1k-unknown-none -g"
 rustc="${rustc} -C target-feature=+mul,+div,+ffl1,+cmov,+addc -C opt-level=s"
 rustc="${rustc} --crate-type rlib"
 
-${rustc} --crate-name core ../src/libcore/lib.rs
-${rustc} --crate-name std_unicode ../src/libstd_unicode/lib.rs
-${rustc} --crate-name alloc ../src/liballoc/lib.rs
-${rustc} --crate-name libc ../src/liblibc_mini/lib.rs
-${rustc} --crate-name unwind ../src/libunwind/lib.rs
-${rustc} -Cpanic=abort --crate-name panic_abort ../src/libpanic_abort/lib.rs
-${rustc} -Cpanic=unwind --crate-name panic_unwind ../src/libpanic_unwind/lib.rs \
+${rustc} --crate-name core src/libcore/lib.rs
+${rustc} --crate-name std_unicode src/libstd_unicode/lib.rs
+${rustc} --crate-name alloc src/liballoc/lib.rs
+${rustc} --crate-name libc src/liblibc_mini/lib.rs
+${rustc} --crate-name unwind src/libunwind/lib.rs
+${rustc} -Cpanic=abort --crate-name panic_abort src/libpanic_abort/lib.rs
+${rustc} -Cpanic=unwind --crate-name panic_unwind src/libpanic_unwind/lib.rs \
   --cfg llvm_libunwind
