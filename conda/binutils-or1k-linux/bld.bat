0a107bcf22e653483f4fe8f31399c5a18372f651
whitequark <whitequark@whitequark.org>
2018-02-28 00:49:36 +0000

    binutils-or1k-linux: give up and just install texinfo.

--- conda/binutils-or1k-linux/bld.bat
+++ conda/binutils-or1k-linux/bld.bat
@@ -2,11 +2,8 @@ set TOOLPREF=mingw-w64-i686-
 set TRIPLE=x86_64-pc-mingw32
 set PATH=%MSYS%\usr\bin;%MSYS%\mingw%ARCH%\bin;%PATH%
 
-pacman -S --noconfirm %TOOLPREF%gcc %TOOLPREF%binutils make autoconf automake libtool pkg-config
+pacman -S --noconfirm %TOOLPREF%gcc %TOOLPREF%binutils make autoconf automake libtool texinfo
 if errorlevel 1 exit 1
-
-rem upstream packaging bug https://sourceware.org/ml/crossgcc/2008-05/msg00071.html
-touch bfd/doc/bfd.info
 
 mkdir build
 cd build
