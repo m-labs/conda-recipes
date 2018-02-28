922e9f7e6f6fbc291e621bd3dc55abb42e797ef2
whitequark <whitequark@whitequark.org>
2018-02-28 00:02:14 +0000

    binutils-or1k-linux: use msys on windows instead of cygwin.

--- conda/binutils-or1k-linux/bld.bat
+++ conda/binutils-or1k-linux/bld.bat
@@ -1,10 +1,20 @@
-FOR /F "tokens=* USEBACKQ" %%F IN (`cygpath -u %PREFIX%`) DO (
-SET var=%%F
-)
-set PREFIX=%var%
-FOR /F "tokens=* USEBACKQ" %%F IN (`cygpath -u %RECIPE_DIR%`) DO (
-SET var=%%F
-)
-set RECIPE_DIR=%var%
-sh %RECIPE_DIR%/build.sh
+set TOOLPREF=mingw-w64-i686-
+set TRIPLE=x86_64-pc-msys
+set PATH=%MSYS%\usr\bin;%MSYS%\mingw%ARCH%\bin;%PATH%
+
+pacman -S --noconfirm %TOOLPREF%gcc %TOOLPREF%binutils make autoconf automake libtool pkg-config
+if errorlevel 1 exit 1
+
+mkdir build
+cd build
+sh ../configure --build=%TRIPLE% ^
+  --prefix="%PREFIX:\=/%/Library" ^
+  --bindir="%PREFIX:\=/%/Scripts" ^
+  --target=or1k-linux
+if errorlevel 1 exit 1
+
+make
+if errorlevel 1 exit 1
+
+make install
 if errorlevel 1 exit 1
